6f563b3496c241c9c42d26858bb4e4a1d3d48ee3
thcheetah777 <thcheetah777@gmail.com>
2022-01-15 17:50:15 -0800

    Create draggable

--- js/Game.js
+++ js/Game.js
@@ -6,14 +6,26 @@ class Game extends Phaser.Scene {
 
   }
   create() {
+    // Sprites
+    game.draggable = this.add.circle(700, 300, 100, 0x4ED4FC);
     game.pointer = this.add.circle(500, 500, 8, 0x000000);
+
+    // Sprite setup
     game.pointer.visible = false;
+    game.draggable.setInteractive();
+
+    // Mouse input
     this.input.on("pointerdown", function () {
       game.pointer.visible = true;
     });
     this.input.on("pointerup", function () {
       game.pointer.visible = false;
     });
+    this.input.on("drag", function (pointer, sprite, x, y) {
+      sprite.x = x;
+      sprite.y = y;
+    });
+    this.input.setDraggable(game.draggable);
   }
   update() {
     game.pointer.x = this.input.mousePointer.x;
