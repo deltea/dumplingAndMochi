0a8fcdd97fe9c499b3b38156a589d349330b4be3
thcheetah777 <thcheetah777@gmail.com>
2022-01-15 17:42:24 -0800

    Show and hide pointer

--- js/Game.js
+++ js/Game.js
@@ -7,8 +7,16 @@ class Game extends Phaser.Scene {
   }
   create() {
     game.pointer = this.add.circle(500, 500, 8, 0x000000);
+    game.pointer.visible = false;
+    this.input.on("pointerdown", function () {
+      game.pointer.visible = true;
+    });
+    this.input.on("pointerup", function () {
+      game.pointer.visible = false;
+    });
   }
   update() {
-
+    game.pointer.x = this.input.mousePointer.x;
+    game.pointer.y = this.input.mousePointer.y;
   }
 }
