353de2d4d37e0670f0c1a274003bf6ca1c645203
thcheetah777 <thcheetah777@gmail.com>
2022-01-15 18:21:39 -0800

    Add pointer trail

--- js/Game.js
+++ js/Game.js
@@ -3,12 +3,18 @@ class Game extends Phaser.Scene {
     super("Game");
   }
   preload() {
-
+    // Load images
+    this.load.image("pointer", "assets/pointer.png");
   }
   create() {
+    const phaser = this;
+
     // Sprites
     game.draggable = this.add.circle(700, 300, 100, 0x4ED4FC);
-    game.pointer = this.add.circle(500, 500, 8, 0x000000);
+    game.pointer = this.add.image(0, 0, "pointer");
+
+    // Sprite groups
+    game.pointerTrail = this.physics.add.staticGroup();
 
     // Sprite setup
     game.pointer.visible = false;
@@ -17,8 +23,10 @@ class Game extends Phaser.Scene {
     // Mouse input
     this.input.on("pointerdown", function () {
       game.pointer.visible = true;
+      game.pointer.down = true;
     });
     this.input.on("pointerup", function () {
+      game.pointer.down = false;
       game.pointer.visible = false;
     });
     this.input.on("drag", function (pointer, sprite, x, y) {
@@ -28,7 +36,18 @@ class Game extends Phaser.Scene {
     this.input.setDraggable(game.draggable);
   }
   update() {
+    if (game.pointer.down) {
+      let trail = game.pointerTrail.create(game.pointer.x, game.pointer.y, "pointer");
+      trail.counter = 0;
+    }
     game.pointer.x = this.input.mousePointer.x;
     game.pointer.y = this.input.mousePointer.y;
+    game.pointerTrail.getChildren().forEach(sprite => {
+      sprite.counter++;
+      sprite.alpha = sprite.counter * 0.15;
+      if (sprite.counter >= 5) {
+        sprite.destroy();
+      }
+    });
   }
 }
