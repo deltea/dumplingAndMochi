f3e883f3405fecccb6e1f6d045edb845b5560658
thcheetah777 <thcheetah777@gmail.com>
2022-01-16 21:13:22 -0800

    Add drag with gravity

--- js/Game.js
+++ js/Game.js
@@ -3,51 +3,42 @@ class Game extends Phaser.Scene {
     super("Game");
   }
   preload() {
-    // Load images
-    this.load.image("pointer", "assets/pointer.png");
+
   }
   create() {
     const phaser = this;
 
     // Sprites
-    game.draggable = this.add.circle(700, 300, 100, 0x4ED4FC);
-    game.pointer = this.add.image(0, 0, "pointer");
-
-    // Sprite groups
-    game.pointerTrail = this.physics.add.staticGroup();
+    game.draggable = this.physics.add.sprite(500, 0, "").setCollideWorldBounds(true);
 
     // Sprite setup
-    game.pointer.visible = false;
     game.draggable.setInteractive();
+    this.input.setDraggable(game.draggable);
 
     // Mouse input
     this.input.on("pointerdown", function () {
-      game.pointer.visible = true;
-      game.pointer.down = true;
+      game.pointerDown = true;
     });
     this.input.on("pointerup", function () {
-      game.pointer.down = false;
-      game.pointer.visible = false;
+      game.pointerDown = false;
     });
     this.input.on("drag", function (pointer, sprite, x, y) {
+      sprite.setGravityY(-config.physics.arcade.gravity.y);
       sprite.x = x;
       sprite.y = y;
     });
-    this.input.setDraggable(game.draggable);
+    this.input.on("dragend", function (pointer, sprite) {
+      sprite.setGravityY(config.physics.arcade.gravity.y);
+      if (pointer.x < sprite.x) {
+        sprite.setVelocityX(-800);
+      } else if (pointer.x > sprite.x) {
+        sprite.setVelocityX(800);
+      } else {
+        sprite.setVelocityX(0);
+      }
+    })
   }
   update() {
-    if (game.pointer.down) {
-      let trail = game.pointerTrail.create(game.pointer.x, game.pointer.y, "pointer");
-      trail.counter = 0;
-    }
-    game.pointer.x = this.input.x;
-    game.pointer.y = this.input.y;
-    game.pointerTrail.getChildren().forEach(sprite => {
-      sprite.counter++;
-      sprite.alpha = sprite.counter * 0.15;
-      if (sprite.counter >= 5) {
-        sprite.destroy();
-      }
-    });
+
   }
 }
